aa7cc219d2d5a9586f380bad0e90182271ebd7fa
killyanhoulettevcel <houlette.killyan.vcel@gmail.com>
2026-03-13 14:43:49 +0100

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -83,16 +83,16 @@ export const authOptions: NextAuthOptions = {
 
     async jwt({ token, user }) {
       if (user) {
-        token.sub  = user.id
-        token.role = (user as any).role || 'client'
+        token['uid']  = user.id
+        token['role'] = (user as any).role || 'client'
       }
       return token
     },
 
     async session({ session, token }) {
       if (session.user) {
-        (session.user as any).id   = token.sub
-        (session.user as any).role = token.role
+        (session.user as any).id   = token['uid']
+        (session.user as any).role = token['role']
       }
       return session
     },
